168c04a0067a5ae46b5f354474468c5a4a1aa430
EyreValor <junnashiro@outlook.com>
2020-02-23 17:42:23 +0800

    init rep

---
+++ src/main/java/cn/glavenus/community/glavenus/GlavenusApplication.java
@@ -0,0 +1,13 @@
+package cn.glavenus.community.glavenus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlavenusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GlavenusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/glavenus/community/glavenus/GlavenusApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.glavenus.community.glavenus;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GlavenusApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
